e9c3030e2fce4ca2b3bdc2b147e2555bddd69b3c
tranquilo <sanbonzakurakogyoshi@gmail.com>
2024-03-11 17:16:37 +0700

    registered tag in templatetag for categories

--- main/views.py
+++ main/views.py
@@ -5,11 +5,10 @@ from goods.models import Categories
 
 
 def index(request):
-    categories = Categories.objects.all()
     context = {
         'title': 'Home - Главная',
-        'content': 'Магазин мебели Home',
-        'categories': categories}
+        'content': 'Магазин мебели Home'
+    }
     return render(request, 'main/index.html', context)
 
 
